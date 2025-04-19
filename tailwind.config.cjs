7d4b3288c81560166fc912a2b55bdfa9f01f3513
hyu164 <tinghyu0304@gmail.com>
2025-04-20 03:45:30 +0800

    fix: ensure tailwindcss and config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // 如果你的程式碼都在 src 裡
+    "./pages/**/*.{js,ts,jsx,tsx}", // 如果有 pages
+    "./components/**/*.{js,ts,jsx,tsx}", // 如果有 components
+    "./app/**/*.{js,ts,jsx,tsx}", // 如果有 app router
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
