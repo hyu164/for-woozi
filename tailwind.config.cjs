c96600ed090e0ceea0dbdabfa68c58118125f620
hyu164 <tinghyu0304@gmail.com>
2025-04-20 03:53:04 +0800

    fix: install tailwindcss and update config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // 如果你的程式碼都在 src 裡
-    "./pages/**/*.{js,ts,jsx,tsx}", // 如果有 pages
-    "./components/**/*.{js,ts,jsx,tsx}", // 如果有 components
-    "./app/**/*.{js,ts,jsx,tsx}", // 如果有 app router
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
